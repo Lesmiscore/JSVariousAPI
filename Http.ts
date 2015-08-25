38e423332485725b78da3db5db3c40f91eecb752
nao20010128nao <nao20010128@gmail.com>
2015-08-25 13:18:31 +0900

    いろいろ

---
+++ Http.ts
@@ -0,0 +1,37 @@
+/* A raw connection class.
+ * You should use "Connect" class.
+ */
+class Http {
+    public static getResp(url: string):string {
+        var urlO = new (eval("java.net.URL"))(url);
+        var con = urlO.openConnection();
+        var reader = new (eval("java.io.InputStreamReader"))(con.getInputStream(), "UTF-8");
+        var data = new (eval("java.io.StringWriter"))();
+        var buf = eval("java.lang.reflect.Array.newInstance")(eval("java.lang.Character.TYPE"), 4096);
+        while (true) {
+            var r = reader.read(buf);
+            if (r <= 0) break;
+            data.write(buf,0,r);
+        }
+        return data.toString();
+    }
+    public static postResp(url: string, send: string) {
+        var urlO = new (eval("java.net.URL"))(url);
+        var con = urlO.openConnection();
+        con.setDoOutput(true);
+        con.setRequestMethod("POST");
+        var writer = new (eval("java.io.OutputStreamWriter"))(con.getOutputStream(), "UTF-8");
+        writer.write(send);
+        writer.flush();
+
+        var reader = new (eval("java.io.InputStreamReader"))(con.getInputStream(), "UTF-8");
+        var data = new (eval("java.io.StringWriter"))();
+        var buf = eval("java.lang.reflect.Array.newInstance")(eval("java.lang.Character.TYPE"), 4096);
+        while (true) {
+            var r = reader.read(buf);
+            if (r <= 0) break;
+            data.write(buf, 0, r);
+        }
+        return data.toString();
+    }
+}
